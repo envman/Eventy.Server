aab06352530d39e9cb6ad827dfc93d5902e8c076
Robert Gill <RobertGill@etech.net>
2015-09-02 19:14:47 +0100

    Added test project

---
+++ EventServer/EventServer.TestClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventServer.TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.Run();
+        }
+
+        private void Run()
+        {
+            DrawSplash();
+
+            Console.ReadLine();
+        }
+
+        private static void DrawSplash()
+        {
+            Console.WriteLine("Test Console");
+            Console.WriteLine("------------");
+
+            Console.WriteLine(@"                     ..-^ ~~~^ -..");
+            Console.WriteLine(@"                   .~             ~.");
+            Console.WriteLine(@"                  (;:             :;)");
+            Console.WriteLine(@"                   (:             :)");
+            Console.WriteLine(@"                     ':._     _.:'");
+            Console.WriteLine(@"                          | |");
+            Console.WriteLine(@"                        (=====)");
+            Console.WriteLine(@"                          | |");
+            Console.WriteLine(@"                          | |");
+            Console.WriteLine(@"                          | |");
+            Console.WriteLine(@"                       ((/   \))");
+        }
+    }
+}
