b8300bc21ddbabe89e3ba6cc9f9fb1de5e50b3a5
Robert Gill <RobertGill@etech.net>
2015-09-15 12:59:26 +0100

    Added user search controller

---
+++ EventServer/EventServer.Api/Controllers/UserSearchController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using EventServer.Api.Models;
+
+namespace EventServer.Api.Controllers
+{
+    public class UserSearchController : ApiController
+    {
+        public IEnumerable<UserHeader> Get(string query)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                return context.Users
+                    .Where(u => u.UserName.ToLower().Contains(query) || u.Email.ToLower().Contains(query))
+                    .Select(u => new UserHeader
+                    {
+                        Id = Guid.Parse(u.Id),
+                        UserName = u.UserName,
+                    });
+            }
+        }
+    }
+}
